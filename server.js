286557b7672d898615ec00948e8daa39694e9bda
Adam McCaffery <adam.mccaffery@ascham.nsw.edu.au>
2022-07-20 08:15:00 +1000

    changing port rules

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ app.get("/settings", (req, res) => {
   res.send(settingsObj);
 });
 
-const port = process.env.PORT || 8080;
+const port = 8080;
 
 app.listen(port);
 console.log(`Server Listening at port ${port}...`);
